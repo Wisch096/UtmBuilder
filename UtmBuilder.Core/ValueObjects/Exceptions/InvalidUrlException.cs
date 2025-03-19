b1b843111fbd7348d05bd3b43a33aca444174f21
Matheus Wisch <wisch.pg@outlook.com>
2025-03-19 15:24:21 -0300

    :sparkles: Feat: Converting url

--- UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -7,7 +7,7 @@ public partial class InvalidUrlException : Exception
     private const string DefaultErrorMessage = "Invalid URL";
     private const string UrlRegexPattern = @"^(https?:\/\/)?([\w\-]+(\.[\w\-]+)+)(:\d+)?(\/[^\s]*)?$";
 
-    private InvalidUrlException(string message = DefaultErrorMessage) : base(message) { }
+    public InvalidUrlException(string message = DefaultErrorMessage) : base(message) { }
 
     public static void ThrowIfInvalidUrl(string address, string message = DefaultErrorMessage)
     {
