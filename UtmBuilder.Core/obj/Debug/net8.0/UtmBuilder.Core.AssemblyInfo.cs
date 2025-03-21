b05fec38b66b81c9562edd22cca1ae16f0c2afce
Matheus Wisch <wisch.pg@outlook.com>
2025-03-21 14:47:53 -0300

    dev

--- UtmBuilder.Core/obj/Debug/net8.0/UtmBuilder.Core.AssemblyInfo.cs
+++ UtmBuilder.Core/obj/Debug/net8.0/UtmBuilder.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UtmBuilder.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24c8d942271e77fbb257eb4b4fa9a61de6861397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b843111fbd7348d05bd3b43a33aca444174f21")]
 [assembly: System.Reflection.AssemblyProductAttribute("UtmBuilder.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UtmBuilder.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
